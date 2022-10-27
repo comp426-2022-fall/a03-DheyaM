c3fd21a01732023ac39049396c7e028c8c22d1a3
Dheya Madhani <dheyam@gmail.com>
2022-10-27 00:07:24 -0400

    added comment

--- bin/cli.js
+++ bin/cli.js
@@ -11,6 +11,7 @@ const args = minimist(process.argv.slice(2));
 const sides = args.sides ? args.sides: 6;
 // default value of dice as 2
 const dice = args.dice ? args.dice: 2;
+// default value of rolls as 1
 const rolls = args.rolls ? args.rolls: 1;
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
