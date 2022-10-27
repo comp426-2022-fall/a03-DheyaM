ffc87a1f0483e24b2a4296e185f0045166b1feed
Dheya Madhani <dheyam@gmail.com>
2022-10-27 00:07:09 -0400

    fixed typo

--- bin/cli.js
+++ bin/cli.js
@@ -9,7 +9,7 @@ const args = minimist(process.argv.slice(2));
 
 // default value of sides as 6
 const sides = args.sides ? args.sides: 6;
-// default value of dice is 2
+// default value of dice as 2
 const dice = args.dice ? args.dice: 2;
 const rolls = args.rolls ? args.rolls: 1;
 
