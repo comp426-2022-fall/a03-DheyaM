3ec42e2c524ec1d8d46adfebf420f2f72ceb3b1f
Dheya Madhani <dheyam@gmail.com>
2022-10-27 00:07:42 -0400

    fixed typos

--- bin/cli.js
+++ bin/cli.js
@@ -7,11 +7,11 @@ import minimist from "minimist";
 // get CLI args
 const args = minimist(process.argv.slice(2));
 
-// default value of sides as 6
+// default value of sides is 6
 const sides = args.sides ? args.sides: 6;
-// default value of dice as 2
+// default value of dice is 2
 const dice = args.dice ? args.dice: 2;
-// default value of rolls as 1
+// default value of rolls is 1
 const rolls = args.rolls ? args.rolls: 1;
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
