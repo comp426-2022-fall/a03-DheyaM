2503308ead5c2980b705a0fe666661d71568c388
Dheya Madhani <dheyam@gmail.com>
2022-10-26 23:57:12 -0400

    exit process

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { roll } from "../lib/roll.js"
+import { roll } from "/lib/roll.js";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
@@ -9,3 +9,4 @@ const dice = args.dice ? args.dice: 2;
 const rolls = args.rolls ? args.rolls: 1;
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
+process.exit(0);
