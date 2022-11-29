8077379e8aac581ed802f58ba8ed19cb6a50b7c4
Dheya Madhani <dheyam@gmail.com>
2022-11-29 09:21:59 -0500

    fixed import and function name called

--- bin/cli.js
+++ bin/cli.js
@@ -3,19 +3,19 @@
 // import minimist to process argv
 import minimist from "minimist";
 // import function created in other folder
-import { Roll } from "../lib/roll.js";
+import Roll from "../lib/roll.js";
 
 // get CLI argv and process them
 const args = minimist(process.argv.slice(2));
 
 // default value of sides is 6: regular dice
-const sides = args.sides ? args.sides: 6;
+const side = args.sides ? args.sides: 6;
 // default value of dice is 2
 const dice = args.dice ? args.dice: 2;
 // default value of rolls is 1
-const rolls = args.rolls ? args.rolls: 1;
+const roll = args.rolls ? args.rolls: 1;
 
 // print output
-console.log(JSON.stringify(roll(sides, dice, rolls)));
+console.log(JSON.stringify(Roll(side, dice, roll)));
 // exit the program at the end
 process.exit(0);
